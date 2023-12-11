990eeefb3bf57601932a41757f329025f8146290
kewalaka <kewalaka@gmail.com>
2023-12-11 18:42:19 +1300

    fix: correct scope attribute

--- main.certificates.tf
+++ main.certificates.tf
@@ -67,7 +67,7 @@ resource "azurerm_key_vault_certificate" "this" {
 
 resource "azurerm_role_assignment" "certificates" {
   for_each                               = local.certificates_role_assignments
-  scope                                  = azurerm_key_vault_certificate.this[each.value.certificate_key].resource_versionless_id
+  scope                                  = azurerm_key_vault_certificate.this[each.value.certificate_key].resource_manager_versionless_id
   role_definition_id                     = strcontains(lower(each.value.role_assignment.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_assignment.role_definition_id_or_name : null
   role_definition_name                   = strcontains(lower(each.value.role_assignment.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_assignment.role_definition_id_or_name
   principal_id                           = each.value.role_assignment.principal_id
