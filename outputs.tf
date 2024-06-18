e291e202347c7fc3be0158091c332fc23f18df03
Jon Chancellor <jchancellor@microsoft.com>
2024-06-18 15:59:11 -0700

    adding back vault_uri output

--- outputs.tf
+++ outputs.tf
@@ -29,3 +29,8 @@ output "secrets_resource_ids" {
     }
   }
 }
+
+output "vault_uri" {
+  description = "The URI of the vault for performing operations on keys and secrets"
+  value       = azurerm_key_vault.this.vault_uri
+}
