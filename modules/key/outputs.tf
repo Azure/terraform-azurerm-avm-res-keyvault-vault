9e50f91683805f752abb0f4d6caf68724f8a2fa6
theccz <ccaprusu@gmail.com>
2024-09-12 16:12:20 +0200

    Update outputs.tf to referece azapi key resource

--- modules/key/outputs.tf
+++ modules/key/outputs.tf
@@ -1,19 +1,19 @@
 output "id" {
   description = "The Key Vault Key ID"
-  value       = azurerm_key_vault_key.this.id
+  value       = azapi_resource.key.id
 }
 
 output "resource_id" {
   description = "The Azure resource id of the secret."
-  value       = azurerm_key_vault_key.this.resource_id
+  value       = azapi_resource.key.resource_id
 }
 
 output "resource_versionless_id" {
   description = "The versionless Azure resource id of the secret."
-  value       = azurerm_key_vault_key.this.resource_versionless_id
+  value       = azapi_resource.key.resource_versionless_id
 }
 
 output "versionless_id" {
   description = "The Base ID of the Key Vault Key"
-  value       = azurerm_key_vault_key.this.versionless_id
+  value       = azapi_resource.key.versionless_id
 }
