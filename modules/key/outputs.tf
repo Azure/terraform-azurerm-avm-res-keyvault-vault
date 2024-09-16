2464ba2bfc0bedbf1261f6c8b46b5aef1b0ce362
theccz <ccaprusu@gmail.com>
2024-09-16 14:34:54 +0200

    Test versionless outputs.tf

--- modules/key/outputs.tf
+++ modules/key/outputs.tf
@@ -8,12 +8,12 @@ output "resource_id" {
   value       = azapi_resource.key.resource_id
 }
 
-output "resource_versionless_id" {
-  description = "The versionless Azure resource id of the secret."
-  value       = azapi_resource.key.resource_versionless_id
-}
+#output "resource_versionless_id" {
+#  description = "The versionless Azure resource id of the secret."
+#  value       = azapi_resource.key.resource_versionless_id
+#}
 
-output "versionless_id" {
-  description = "The Base ID of the Key Vault Key"
-  value       = azapi_resource.key.versionless_id
-}
+#output "versionless_id" {
+#  description = "The Base ID of the Key Vault Key"
+#  value       = azapi_resource.key.versionless_id
+#}
