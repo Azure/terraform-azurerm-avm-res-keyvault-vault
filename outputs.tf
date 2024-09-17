0cd8c312fadfdb5e20113ea1681fc5d5fe622875
theccz <ccaprusu@gmail.com>
2024-09-17 16:05:37 +0200

    Update outputs to point to resource id

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-#output "keys_resource_ids" {
-#  description = "A map of key keys to resource ids."
-#  value = { for kk, kv in module.keys : kk => {
+output "keys_resource_ids" {
+  description = "A map of key keys to resource ids."
+  value = { for kk, kv in module.keys : kk => {
 #    resource_id             = kv.resource_id
 #    resource_versionless_id = kv.resource_versionless_id
-#    id                      = kv.id
+    id                      = kv.id
 #    versionless_id          = kv.versionless_id
-#    }
-#  }
-#}
+    }
+  }
+}
 
 output "private_endpoints" {
   description = "A map of private endpoints. The map key is the supplied input to var.private_endpoints. The map value is the entire azurerm_private_endpoint resource."
@@ -19,16 +19,16 @@ output "resource_id" {
   value       = azurerm_key_vault.this.id
 }
 
-#output "secrets_resource_ids" {
-#  description = "A map of secret keys to resource ids."
-#  value = { for sk, sv in module.secrets : sk => {
+output "secrets_resource_ids" {
+  description = "A map of secret keys to resource ids."
+  value = { for sk, sv in module.secrets : sk => {
 #    resource_id             = sv.resource_id
 #    resource_versionless_id = sv.resource_versionless_id
-#    id                      = sv.id
+    id                      = sv.id
 #    versionless_id          = sv.versionless_id
-#    }
-#  }
-#}
+    }
+  }
+}
 
 output "uri" {
   description = "The URI of the vault for performing operations on keys and secrets"
