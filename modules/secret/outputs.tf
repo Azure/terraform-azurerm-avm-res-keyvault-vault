a1400a105f25a3460344ec9f1bbb0f13ff3e2cfb
theccz <ccaprusu@gmail.com>
2024-09-24 08:26:19 +0200

    Update outputs to match the existing avm outputs

--- modules/secret/outputs.tf
+++ modules/secret/outputs.tf
@@ -3,17 +3,23 @@ output "id" {
   value       = azapi_resource.secret.id
 }
 
-#output "resource_id" {
-#  description = "The Azure resource id of the secret."
-#  value       = azapi_resource.key.output.properties.resource_id
-#}
+output "resource_id" {
+  description = "The Azure resource id of the secret."
+  value = "${azapi_resource.secret.output.id}/${
+    regex_replace(
+      jsondecode(azapi_resource.secret.output).properties.secretUriWithVersion, 
+      "/(.+)$",
+      "$1"
+    )
+  }" 
+}
 
-#output "resource_versionless_id" {
-# description = "The versionless Azure resource id of the secret."
-# value       = azapi_resource.secret.resource_versionless_id
-#}
+output "resource_versionless_id" {
+ description = "The versionless Azure resource id of the secret."
+ value       = azapi_resource.secret.output.properties.id
+}
 
-#output "versionless_id" {
-#  description = "The Base ID of the Key Vault Secret"
-#  value       = azapi_resource.secret.versionless_id
-#}
+output "versionless_id" {
+  description = "The Base ID of the Key Vault Secret"
+  value       = azapi_resource.secret.output.properties.secretUri
+}
