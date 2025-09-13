bee44a6cbc3d5ba8fbd3dd9f0efd93d2ed08b78f
jonas-budde <jonas.budde@outlook.de>
2025-09-13 13:09:55 +0200

    update the minimal needed azurerm version for new field

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.117, < 5.0"
+      version = ">= 4.20, < 5.0"
     }
     modtm = {
       source  = "azure/modtm"
