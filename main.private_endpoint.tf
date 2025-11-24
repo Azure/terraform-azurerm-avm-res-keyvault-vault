73f08d7cbc0f9188f8414c2348d2f0db4714f1b5
Rob Sissons <132371216+robsissons-contino@users.noreply.github.com>
2025-11-24 10:02:47 +0000

    fix: asg association and asg var description

--- main.private_endpoint.tf
+++ main.private_endpoint.tf
@@ -72,5 +72,5 @@ resource "azurerm_private_endpoint_application_security_group_association" "this
   for_each = local.private_endpoint_application_security_group_associations
 
   application_security_group_id = each.value.asg_resource_id
-  private_endpoint_id           = azurerm_private_endpoint.this[each.value.pe_key].id
+  private_endpoint_id           = var.private_endpoints_manage_dns_zone_group ? azurerm_private_endpoint.this[each.value.pe_key].id : azurerm_private_endpoint.this_unmanaged_dns_zone_groups[each.value.pe_key].id
 }
